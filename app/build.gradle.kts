317ac9f02cb2016dd23df2983217ac31e802db8e
anubhav.sahoo <anubhav.sahoo@mobisy.com>
2025-06-07 00:40:45 +0530

    Added dependencies for amazon transcribe

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,4 +56,8 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    implementation(libs.aws.android.sdk.core)
+    implementation(libs.aws.android.sdk.transcribe)
+    implementation(libs.aws.android.sdk.s3)
 }
