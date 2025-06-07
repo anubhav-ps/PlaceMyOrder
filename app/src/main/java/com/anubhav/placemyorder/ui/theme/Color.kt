6359fc127f28deedb2ee02c1d072eb8c50d0cabd
anubhav.sahoo <anubhav.sahoo@mobisy.com>
2025-06-07 11:25:19 +0530

    UI Theme

--- app/src/main/java/com/anubhav/placemyorder/ui/theme/Color.kt
+++ app/src/main/java/com/anubhav/placemyorder/ui/theme/Color.kt
@@ -9,3 +9,21 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+
+val white_10 = Color(0xFFFFFFFF)
+val white_20 = Color(0xFFFDFEFF)
+val white_30 = Color(0xFFFCFCFC)
+val white_40 = Color(0xFFF5F5F5)
+val white_50 = Color(0xFFF3F3F3)
+
+val gray_10 = Color(0xFF585656)
+val gray_20 = Color(0xFF8C8C8C)
+val gray_30 = Color(0xFFD9D9D9)
+val gray_40 = Color(0xFFD9D9D9)
+
+
+val blue_10 = Color(0xFFB4D4FF)
+
+
+val red_10 = Color(0xFFFE7772)
--- app/src/main/java/com/anubhav/placemyorder/ui/theme/Theme.kt
+++ app/src/main/java/com/anubhav/placemyorder/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package com.anubhav.placemyorder.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
@@ -14,13 +13,15 @@ import androidx.compose.ui.platform.LocalContext
 private val DarkColorScheme = darkColorScheme(
     primary = Purple80,
     secondary = PurpleGrey80,
-    tertiary = Pink80
+    tertiary = Pink80,
+    background = white_20,
 )
 
 private val LightColorScheme = lightColorScheme(
     primary = Purple40,
     secondary = PurpleGrey40,
-    tertiary = Pink40
+    tertiary = Pink40,
+    background = white_20
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
@@ -38,7 +39,7 @@ fun PlaceMyOrderTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
-    content: @Composable () -> Unit
+    content: @Composable () -> Unit,
 ) {
     val colorScheme = when {
         dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
